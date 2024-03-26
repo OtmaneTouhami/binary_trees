6bb46e2a3914df17d1192e295ae1cff390923e9f
OtmaneTouhami <otmanetouhami01@gmail.com>
2024-03-26 20:41:30 +0000

    Binary trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,15 +10,15 @@
  */
 binary_tree_t *binary_tree_node_1(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-new_node = malloc(sizeof(binary_tree_t));
-if (new_node == NULL)
-return (NULL);
-new_node->parent = parent;
-new_node->n = value;
-new_node->left = new_node->right = NULL;
-return (new_node);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = new_node->right = NULL;
+	return (new_node);
 }
 
 /**
@@ -33,16 +33,16 @@ return (new_node);
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *left_child;
+	binary_tree_t *left_child;
 
-if (parent == NULL)
-return (NULL);
-left_child = binary_tree_node_1(parent, value);
-if (left_child == NULL)
-return (NULL);
-left_child->left = parent->left;
-if (left_child->left != NULL)
-left_child->left->parent = left_child;
-parent->left = left_child;
-return (left_child);
+	if (parent == NULL)
+		return (NULL);
+	left_child = binary_tree_node_1(parent, value);
+	if (left_child == NULL)
+		return (NULL);
+	left_child->left = parent->left;
+	if (left_child->left != NULL)
+		left_child->left->parent = left_child;
+	parent->left = left_child;
+	return (left_child);
 }
